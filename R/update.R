077c56a35c9816d58803bf4298ba35f3ae7a367c
etiennebacher <etienne.bacher@protonmail.com>
2022-02-12 21:24:18 +0100

    fix update_docs with mkdocs

--- R/update.R
+++ R/update.R
@@ -16,16 +16,21 @@
 
 update_docs <- function() {
 
+  good_path <- doc_path()
+
+  # Update README
   update_file("README.md")
   move_img_readme()
   replace_img_paths_readme()
-  reformat_md("docs/README.md")
+  reformat_md(paste0(good_path, "/README.md"))
 
+  # Update changelog
   update_file("NEWS.md")
 
-
+  # Update functions reference
   make_reference()
 
+  # Finish
   message_validate("Documentation updated. See `?altdoc::update_docs` to know
                    what files are concerned.")
 
@@ -35,14 +40,18 @@ update_docs <- function() {
 update_file <- function(filename) {
 
   orig_file <- filename
-  docs_file <- paste0("docs/", filename)
+  good_path <- doc_path()
+  docs_file <- paste0(good_path, "/", filename)
   if (fs::file_exists(filename)) {
     fs::file_copy(orig_file, docs_file, overwrite = TRUE)
     if (filename == "NEWS.md") {
+      if (!fs::file_exists(docs_file)) {
+        message_info("NEWS.md was imported for the first time. You should also update {.code docs/mkdocs.yml}.")
+      }
       changelog <- readLines("NEWS.md", warn = FALSE)
       changelog <- gsub("^## ", "### ", changelog)
       changelog <- gsub("^# ", "## ", changelog)
-      writeLines(changelog, "docs/NEWS.md")
+      writeLines(changelog, docs_file)
     }
   }
 }
