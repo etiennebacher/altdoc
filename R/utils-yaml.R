fdeca0e350209cccebcb2aaffd9b1f62f56f42f8
etiennebacher <etienne.bacher@protonmail.com>
2022-02-19 21:56:10 +0100

    add functions to modify yaml, convert vignettes and render them to md, and add sections for docute

---
+++ R/utils-yaml.R
@@ -0,0 +1,66 @@
+# Modify yaml of vignettes to remove HTML formats in output, remove all
+# vignette parameters (starting with %\Vignette), and add github_document
+# in output formats.
+
+modify_yaml <- function(filename) {
+
+  # Extract yaml from Rmd
+  x <- readLines(filename, warn = FALSE)
+  yaml_limits <- grep("---", x)[c(1,2)]
+  yaml <- x[yaml_limits[1]:yaml_limits[2]]
+  new_vignette <- x[-c(yaml_limits[1]:yaml_limits[2])]
+
+  # Remove the vignette-specific lines
+  yaml <- yaml[!startsWith(yaml, "vignette: >")]
+  yaml <- yaml[!startsWith(yaml, "  %\\Vignette")]
+  yaml <- paste(yaml, collapse = "\n")
+
+  # Save a tmp file (required by yaml::read_yaml)
+  tmp <- tempfile()
+  writeLines(yaml, tmp)
+
+  # Get yaml as a list, remove HTML outputs (but keep pdf if there are some)
+  original_yaml <- yaml::read_yaml(tmp)
+
+  if (length(original_yaml$output) == 1) {
+    if (grepl("html_", original_yaml$output))
+      original_yaml$output <- "github_document: default"
+  } else if (length(original_yaml$output) > 1) { # if other outputs present (e.g pdf), keep them
+    html_outputs <- names(original_yaml$output)
+    html_outputs <- grep("html_", html_outputs)
+    original_yaml$output[[html_outputs]] <- NULL
+    original_yaml$output[["github_document"]] <- "default"
+
+    # the first output must be github document
+    original_yaml$output <- rev(original_yaml$output)
+  }
+  # necessary for some Rmd files
+  original_yaml$always_allow_html <- TRUE
+
+  # yaml::as.yaml introduces a line break in yaml title if it's too long
+  # so I need to fix it manually by splitting the yaml in two parts
+  new_yaml_1 <- yaml::as.yaml(original_yaml$title)
+  new_yaml_1 <- paste0("title: ", new_yaml_1)
+  new_yaml_1 <- gsub("\\\n", "", new_yaml_1)
+  if (length(original_yaml) > 1) {
+    new_yaml_2 <- yaml::as.yaml(original_yaml[grep("title", names(original_yaml), invert = TRUE)])
+  } else {
+    new_yaml_2 <- NULL
+  }
+
+
+  new_yaml <- paste0(new_yaml_1, "\n", new_yaml_2)
+
+  # Finish the new yaml and add it back to the vignette
+  if (!grepl("github_document", new_yaml)) {
+    new_yaml <- paste0(new_yaml, "output:\n  github_document: default\n")
+  }
+  new_yaml <- gsub("'github_document: default'\\\n",
+                   "\\\n  github_document: default\\\n",
+                   new_yaml)
+  new_yaml <- gsub("\\\n$", "", new_yaml)
+  new_yaml <- c("---", new_yaml, "---\n")
+
+  new_vignette <- c(new_yaml, new_vignette)
+  writeLines(new_vignette, filename)
+}
--- R/vignettes.R
+++ R/vignettes.R
@@ -11,66 +11,63 @@
 
 transform_vignettes <- function() {
 
-  if (!file.exists("vignettes") || folder_is_empty("vignettes")) {
+  if (!file.exists("vignettes") | folder_is_empty("vignettes")) {
     message_info("No vignettes to transform.")
-  } else {
+    return(invisible())
+  }
 
-    list_vignettes <- list.files("vignettes", pattern = ".Rmd")
+  good_path <- doc_path()
+  articles_path <- paste0(good_path, "/articles")
 
-    if (!file.exists("docs/articles")) {
-      fs::dir_create("docs/articles")
-    }
+  vignettes <- list.files("vignettes", pattern = ".Rmd")
 
-    for (i in seq_along(list_vignettes)) {
+  if (!file.exists(articles_path)) {
+    fs::dir_create(articles_path)
+  }
 
-      first_vignette <- paste0("vignettes/", list_vignettes[i])
-      second_vignette <- paste0("docs/articles/", list_vignettes[i])
+  n <- length(vignettes)
+  i <- 0
+  cli::cli_progress_step("Converting vignettes: {i}/{n}", spinner = TRUE)
 
-      if (vignettes_differ(first_vignette, second_vignette)) {
+  for (i in seq_along(vignettes)) {
+    origin <- paste0("vignettes/", vignettes[i])
+    destination <- paste0("docs/articles/", vignettes[i])
 
-        fs::file_copy(
-          first_vignette,
-          second_vignette,
-          overwrite = TRUE
+    if (vignettes_differ(origin, destination)) {
+
+      fs::file_copy(origin, destination, overwrite = TRUE)
+      modify_yaml(destination)
+      output_file <- paste0(substr(vignettes[i], 1, nchar(vignettes[i])-4), ".md")
+
+      suppressMessages(
+        rmarkdown::render(
+          destination,
+          output_dir = articles_path,
+          output_file = output_file,
+          quiet = TRUE
         )
+      )
 
-        original_vignette <- readLines(second_vignette, warn = FALSE)
-
-        start_of_output <- which(startsWith(original_vignette, "output:"))
-        end_of_output <- which(startsWith(original_vignette, "vignette:")) - 1
-        output_chunk <- original_vignette[start_of_output:end_of_output]
-
-        # Remove output chunk and insert the new output
-        modified_vignette <- original_vignette[-c(start_of_output:end_of_output)]
-        modified_vignette[start_of_output] <- paste0(
-          "output:\n  rmarkdown::github_document: \n    html_preview: false\n",
-          modified_vignette[start_of_output]
-        )
-        modified_vignette <- paste(modified_vignette, collapse = "\n")
-        cat(modified_vignette, file = second_vignette)
-
-        # Store vignettes in .md format in "docs/articles"
-        output_file <- paste0(
-          substr(list_vignettes[i], 1, nchar(list_vignettes[i])-4),
-          ".md"
-        )
-
-        suppressMessages(
-          rmarkdown::render(
-            second_vignette,
-            output_dir = "docs/articles",
-            output_file = output_file,
-            quiet = TRUE
-          )
-        )
-
+      ### If title too long, it was cut in several lines but only the last
+      ### one is read by docute so need to paste the title back together
+      new_vignette <- readLines(gsub("\\.Rmd", "\\.md", destination), warn = FALSE)
+      title_sep <- grep("=====", new_vignette)
+      if (length(title_sep) == 1) {
+        title <- new_vignette[1:title_sep-1]
+        title <- paste(title, collapse = " ")
+        new_vignette <- new_vignette[-c(1:title_sep-1)]
+        new_vignette <- c(title, new_vignette)
+        writeLines(new_vignette, gsub("\\.Rmd", "\\.md", destination))
       }
 
+      cli::cli_progress_update()
     }
-
-    message_validate("Vignettes have been converted and put in 'docs/articles'.")
-
   }
+
+  cli::cli_progress_done()
+  message_validate(paste0("Vignettes have been converted and put in '",
+                          articles_path, "'."))
+
 
 }
 
@@ -89,7 +86,7 @@ transform_vignettes <- function() {
 
 vignettes_differ <- function(x, y) {
 
-  if (!file.exists(x) || !file.exists(y)) {
+  if (!file.exists(x) | !file.exists(y)) {
     return(TRUE)
   }
 
@@ -100,4 +97,61 @@ vignettes_differ <- function(x, y) {
   y_content <- gsub("---(.*?)---", "", paste(y_file, collapse = " "))
 
   return(!identical(x_content, y_content))
+}
+
+
+# Get titles and filenames of the vignettes
+# This is used to update the sidebar/navbar in the docs
+
+get_vignettes_titles <- function() {
+
+  if (!file.exists("vignettes") | folder_is_empty("vignettes")) {
+    message_info("No vignettes to transform.")
+    return(invisible())
+  }
+
+  good_path <- doc_path()
+  vignettes <- list.files(paste0(good_path, "/articles"), pattern = ".Rmd")
+
+  vignettes_title <- data.frame(title = NULL, link = NULL)
+  for (i in seq_along(vignettes)) {
+    x <- readLines(paste0(good_path, "/articles/", vignettes[i]), warn = FALSE)
+    title <- x[startsWith(x, "title: ")]
+    title <- gsub("title: ", "", title)
+    vignettes_title[i, "title"] <- title
+
+    link <- paste0("/articles/", vignettes[i])
+    link <- gsub("\\.Rmd", "\\.md", link)
+    vignettes_title[i, "link"] <- link
+  }
+
+  return(
+    jsonlite::toJSON(vignettes_title, pretty = TRUE)
+  )
+}
+
+
+add_vignettes <- function(doctype) {
+
+  if (doctype == "docute") {
+    original_index <- readLines("docs/index.html", warn = FALSE)
+    home_line <- which(grepl("\\{title: 'Home', link: '/'\\}", original_index))
+
+    original_index[home_line] <- paste0(
+      original_index[home_line],
+      "\n{
+					   title: \"Articles\",
+					   children:",
+      get_vignettes_titles(),
+      "},\n"
+    )
+
+    writeLines(original_index, "docs/index.html")
+
+  } else if (doctype == "docsify") {
+
+  } else if (doctype == "mkdocs") {
+
+  }
+
 }
