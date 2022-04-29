4df1f90cebba0fbb0e3736ea6587b5342b343652
etiennebacher <etienne.bacher@protonmail.com>
2022-04-29 10:59:30 +0200

    tests: add tests for license and CoC in update_docs()

--- tests/testthat/test-update_docs.R
+++ tests/testthat/test-update_docs.R
@@ -1,3 +1,5 @@
+# README --------------------------------------------------------
+
 test_that("docute: update_docs updates correctly the README", {
   create_local_package()
   use_docute()
@@ -35,6 +37,9 @@ test_that("mkdocs: update_docs updates correctly the README", {
   expect_true(identical(readme1, readme2))
 })
 
+
+
+# NEWS --------------------------------------------------------
 
 test_that("docute: update_docs updates correctly the NEWS", {
   create_local_package()
@@ -82,29 +87,32 @@ test_that("mkdocs: update_docs updates correctly the NEWS", {
   expect_true(identical(news1, news2))
 })
 
-test_that("docsify: update_docs shows message when NEWS didn't exist", {
+test_that("docsify: update_docs works when NEWS didn't exist", {
   create_local_package()
   use_docsify()
   usethis::use_news_md()
   expect_message(update_docs(),
                  regexp = "'NEWS / Changelog' was imported for the first time.")
+  expect_true(fs::file_exists("docs/NEWS.md"))
 })
 
-test_that("docute: update_docs shows message when NEWS didn't exist", {
+test_that("docute: update_docs works when NEWS didn't exist", {
   create_local_package()
   use_docute()
   usethis::use_news_md()
   expect_message(update_docs(),
                  regexp = "'NEWS / Changelog' was imported for the first time.")
+  expect_true(fs::file_exists("docs/NEWS.md"))
 })
 
-test_that("mkdocs: update_docs shows message when NEWS didn't exist", {
+test_that("mkdocs: update_docs works when NEWS didn't exist", {
   skip_mkdocs()
   create_local_package()
   use_mkdocs()
   usethis::use_news_md()
   expect_message(update_docs(),
                  regexp = "'NEWS / Changelog' was imported for the first time.")
+  expect_true(fs::file_exists("docs/NEWS.md"))
 })
 
 test_that("docsify: update_docs shows message when NEWS doesn't exist", {
@@ -128,6 +136,213 @@ test_that("mkdocs: update_docs shows message when NEWS doesn't exist", {
   expect_message(update_docs(),
                  regexp = "No 'NEWS / Changelog' to include.")
 })
+
+
+
+
+# CODE OF CONDUCT --------------------------------------------------------
+
+test_that("docute: update_docs updates correctly the CoC", {
+  create_local_package()
+  usethis::use_code_of_conduct("etienne.bacher@protonmail.com")
+  writeLines("Hello", con = "CODE_OF_CONDUCT.md")
+  use_docute()
+  writeLines("Hello again", con = "CODE_OF_CONDUCT.md")
+  news1 <- readLines("CODE_OF_CONDUCT.md", warn = FALSE)
+  news2 <- readLines("docs/CODE_OF_CONDUCT.md", warn = FALSE)
+  expect_false(identical(news1, news2))
+
+  update_docs()
+  news2 <- readLines("docs/CODE_OF_CONDUCT.md", warn = FALSE)
+  expect_true(identical(news1, news2))
+})
+
+test_that("docsify: update_docs updates correctly the CoC", {
+  create_local_package()
+  usethis::use_code_of_conduct("etienne.bacher@protonmail.com")
+  writeLines("Hello", con = "CODE_OF_CONDUCT.md")
+  use_docsify()
+  writeLines("Hello again", con = "CODE_OF_CONDUCT.md")
+  news1 <- readLines("CODE_OF_CONDUCT.md", warn = FALSE)
+  news2 <- readLines("docs/CODE_OF_CONDUCT.md", warn = FALSE)
+  expect_false(identical(news1, news2))
+
+  update_docs()
+  news2 <- readLines("docs/CODE_OF_CONDUCT.md", warn = FALSE)
+  expect_true(identical(news1, news2))
+})
+
+test_that("mkdocs: update_docs updates correctly the CoC", {
+  skip_mkdocs()
+  create_local_package()
+  usethis::use_code_of_conduct("etienne.bacher@protonmail.com")
+  writeLines("Hello", con = "CODE_OF_CONDUCT.md")
+  use_mkdocs()
+  writeLines("Hello again", con = "CODE_OF_CONDUCT.md")
+  news1 <- readLines("CODE_OF_CONDUCT.md", warn = FALSE)
+  news2 <- readLines("docs/docs/CODE_OF_CONDUCT.md", warn = FALSE)
+  expect_false(identical(news1, news2))
+
+  update_docs()
+  news2 <- readLines("docs/docs/CODE_OF_CONDUCT.md", warn = FALSE)
+  expect_true(identical(news1, news2))
+})
+
+test_that("docsify: update_docs works when CoC didn't exist", {
+  create_local_package()
+  use_docsify()
+  usethis::use_code_of_conduct("etienne.bacher@protonmail.com")
+  expect_message(update_docs(),
+                 regexp = "'Code of Conduct' was imported for the first time.")
+  expect_true(fs::file_exists("docs/CODE_OF_CONDUCT.md"))
+})
+
+test_that("docute: update_docs works when CoC didn't exist", {
+  create_local_package()
+  use_docute()
+  usethis::use_code_of_conduct("etienne.bacher@protonmail.com")
+  expect_message(update_docs(),
+                 regexp = "'Code of Conduct' was imported for the first time.")
+  expect_true(fs::file_exists("docs/CODE_OF_CONDUCT.md"))
+})
+
+test_that("mkdocs: update_docs works when CoC didn't exist", {
+  skip_mkdocs()
+  create_local_package()
+  use_mkdocs()
+  usethis::use_code_of_conduct("etienne.bacher@protonmail.com")
+  expect_message(update_docs(),
+                 regexp = "'Code of Conduct' was imported for the first time.")
+  expect_true(fs::file_exists("docs/CODE_OF_CONDUCT.md"))
+})
+
+test_that("docsify: update_docs shows message when CoC doesn't exist", {
+  create_local_package()
+  use_docsify()
+  expect_message(update_docs(),
+                 regexp = "No 'Code of Conduct' to include.")
+})
+
+test_that("docute: update_docs shows message when CoC doesn't exist", {
+  create_local_package()
+  use_docute()
+  expect_message(update_docs(),
+                 regexp = "No 'Code of Conduct' to include.")
+})
+
+test_that("mkdocs: update_docs shows message when CoC doesn't exist", {
+  skip_mkdocs()
+  create_local_package()
+  use_mkdocs()
+  expect_message(update_docs(),
+                 regexp = "No 'Code of Conduct' to include.")
+})
+
+
+
+
+
+# LICENSE --------------------------------------------------------
+
+test_that("docute: update_docs updates correctly the License", {
+  create_local_package()
+  usethis::use_mit_license("etienne.bacher@protonmail.com")
+  writeLines("Hello", con = "LICENSE.md")
+  use_docute()
+  writeLines("Hello again", con = "LICENSE.md")
+  news1 <- readLines("LICENSE.md", warn = FALSE)
+  news2 <- readLines("docs/LICENSE.md", warn = FALSE)
+  expect_false(identical(news1, news2))
+
+  update_docs()
+  news2 <- readLines("docs/LICENSE.md", warn = FALSE)
+  expect_true(identical(news1, news2))
+})
+
+test_that("docsify: update_docs updates correctly the License", {
+  create_local_package()
+  usethis::use_mit_license("etienne.bacher@protonmail.com")
+  writeLines("Hello", con = "LICENSE.md")
+  use_docsify()
+  writeLines("Hello again", con = "LICENSE.md")
+  news1 <- readLines("LICENSE.md", warn = FALSE)
+  news2 <- readLines("docs/LICENSE.md", warn = FALSE)
+  expect_false(identical(news1, news2))
+
+  update_docs()
+  news2 <- readLines("docs/LICENSE.md", warn = FALSE)
+  expect_true(identical(news1, news2))
+})
+
+test_that("mkdocs: update_docs updates correctly the License", {
+  skip_mkdocs()
+  create_local_package()
+  usethis::use_mit_license("etienne.bacher@protonmail.com")
+  writeLines("Hello", con = "LICENSE.md")
+  use_mkdocs()
+  writeLines("Hello again", con = "LICENSE.md")
+  news1 <- readLines("LICENSE.md", warn = FALSE)
+  news2 <- readLines("docs/docs/LICENSE.md", warn = FALSE)
+  expect_false(identical(news1, news2))
+
+  update_docs()
+  news2 <- readLines("docs/docs/LICENSE.md", warn = FALSE)
+  expect_true(identical(news1, news2))
+})
+
+test_that("docsify: update_docs works when License didn't exist", {
+  create_local_package()
+  use_docsify()
+  usethis::use_mit_license("etienne.bacher@protonmail.com")
+  expect_message(update_docs(),
+                 regexp = "'License / Licence' was imported for the first time.")
+  expect_true(fs::file_exists("docs/LICENSE.md"))
+})
+
+test_that("docute: update_docs works when License didn't exist", {
+  create_local_package()
+  use_docute()
+  usethis::use_mit_license("etienne.bacher@protonmail.com")
+  expect_message(update_docs(),
+                 regexp = "'License / Licence' was imported for the first time.")
+  expect_true(fs::file_exists("docs/LICENSE.md"))
+})
+
+test_that("mkdocs: update_docs works when License didn't exist", {
+  skip_mkdocs()
+  create_local_package()
+  use_mkdocs()
+  usethis::use_mit_license("etienne.bacher@protonmail.com")
+  expect_message(update_docs(),
+                 regexp = "'License / Licence' was imported for the first time.")
+  expect_true(fs::file_exists("docs/LICENSE.md"))
+})
+
+test_that("docsify: update_docs shows message when License doesn't exist", {
+  create_local_package()
+  use_docsify()
+  expect_message(update_docs(),
+                 regexp = "No 'License / Licence' to include.")
+})
+
+test_that("docute: update_docs shows message when License doesn't exist", {
+  create_local_package()
+  use_docute()
+  expect_message(update_docs(),
+                 regexp = "No 'License / Licence' to include.")
+})
+
+test_that("mkdocs: update_docs shows message when License doesn't exist", {
+  skip_mkdocs()
+  create_local_package()
+  use_mkdocs()
+  expect_message(update_docs(),
+                 regexp = "No 'License / Licence' to include.")
+})
+
+
+
+# VIGNETTES --------------------------------------------------------
 
 test_that("docute: update_docs also transform new/modified vignettes if specified", {
   # setup
