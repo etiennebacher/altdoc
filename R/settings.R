5c432cfbc4d9f86b449e204f89bae8e49216772f
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2023-11-14 22:05:06 -0500

    altdoc variables in separate file

--- R/settings.R
+++ R/settings.R
@@ -1,35 +1,3 @@
-.substitute_altdoc_variables <- function(x, filename, path = ".") {
-    x <- gsub("\\$ALTDOC_VERSION", packageVersion("altdoc"), x)
-
-    # DESCRIPTION file
-    fn <- fs::path_join(c(path, "DESCRIPTION"))
-    if (fs::file_exists(fn)) {
-
-        # before $ALTDOC_PACKAGE_URL
-        urls <- desc::desc_get_urls()
-        urls <- Filter(function(x) grepl("github.com", x), urls)
-        if (length(urls) > 0) {
-            x <- gsub("\\$ALTDOC_PACKAGE_URL_GITHUB", urls[1], x)
-        } else {
-            x <- x[!grepl("\\$ALTDOC_PACKAGE_URL_GITHUB", x)]
-        }
-
-        x <- gsub("\\$ALTDOC_PACKAGE_NAME", desc::desc_get("Package"), x)
-        x <- gsub("\\$ALTDOC_PACKAGE_VERSION", desc::desc_get("Version"), x)
-        x <- gsub("\\$ALTDOC_PACKAGE_URL", desc::desc_get_urls()[1], x)
-
-
-    } else {
-        x <- gsub("\\$ALTDOC_PACKAGE_NAME", "", x)
-        x <- gsub("\\$ALTDOC_PACKAGE_VERSION", "", x)
-        x <- x[grepl("\\$ALTDOC_PACKAGE_URL_GITHUB")] # before the other one
-        x <- gsub("\\$ALTDOC_PACKAGE_URL", "", x)
-    }
-
-    return(x)
-}
-
-
 .import_settings <- function(path = ".", doctype = "docsify") {
 
     # copy all files from altdoc/ into docs/
