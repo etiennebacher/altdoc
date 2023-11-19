543161c1ef4c1a3d0f255e8b5d6626eb70471e85
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2023-11-19 08:01:16 -0500

    obsolete functions

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -7,18 +7,6 @@ test_that(".folder_is_empty() works", {
   expect_false(.folder_is_empty("docs"))
 })
 
-test_that(".check_docs_exists() works", {
-  create_local_package()
-  expect_silent(.check_docs_exists())
-
-  fs::dir_create("docs")
-  expect_silent(.check_docs_exists())
-
-  ## Test disabled because we now prompt for overwriting instead of erroring
-  # fs::file_create("docs/test.md")
-  # expect_error(.check_docs_exists())
-})
-
 test_that(".pkg_name() works", {
   create_local_package()
   expect_true(is.character(.pkg_name(getwd())))
