775229c9dbd7b63fb417e02425d7f4c96d926ad5
Etienne Bacher <52219252+etiennebacher@users.noreply.github.com>
2023-11-23 15:48:33 +0100

    use r2u, deal with mkdocs installation

--- R/create_workflow.R
+++ R/create_workflow.R
@@ -6,5 +6,18 @@ create_workflow <- function(path = ".") {
   src <- system.file("misc/altdoc.yml", package = "altdoc")
   tar <- ".github/workflows/altdoc.yml"
   fs::file_copy(src, tar)
+
+  # Deal with mkdocs installation in workflow
+  doctype <- .doc_type(path)
+  workflow <- .readlines(tar)
+  start <- grep("\\$ALTDOC_MKDOCS_START", workflow)
+  end <- grep("\\$ALTDOC_MKDOCS_END", workflow)
+  if (doctype == "mkdocs") {
+    workflow <- workflow[-c(start, end)]
+  } else {
+    workflow <- workflow[-(start:end)]
+  }
+  writeLines(workflow, tar)
+
   cli::cli_alert_success("{.file .github/workflows/altdoc.yml} created.")
 }
