483bc12d22e524c5cf0fa2b399df053b87f68b3e
Etienne Bacher <52219252+etiennebacher@users.noreply.github.com>
2023-11-23 09:13:49 +0100

    remove the step setup_docs()

--- R/create_workflow.R
+++ R/create_workflow.R
@@ -6,11 +6,5 @@ create_workflow <- function(path = ".") {
   src <- system.file("misc/altdoc.yml", package = "altdoc")
   tar <- ".github/workflows/altdoc.yml"
   fs::file_copy(src, tar)
-
-  doctype <- .doc_type(path)
-  workflow <- .readlines(tar)
-  workflow <- gsub("\\$ALTDOC_TOOL", doctype, workflow)
-
-  writeLines(workflow, tar)
   cli::cli_alert_success("{.file .github/workflows/altdoc.yml} created.")
 }
