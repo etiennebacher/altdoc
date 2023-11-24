8e5430e01f0d36a15cddfe8fbe06136c1fdfe208
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2023-11-24 16:39:54 -0500

    add prefer-html: true to quarto preamble when there is no preamble

--- R/qmd2md.R
+++ R/qmd2md.R
@@ -1,4 +1,6 @@
 .qmd2md <- function(source_file, tar_dir, verbose = FALSE) {
+
+
   if (missing(source_file) || !file.exists(source_file)) {
     stop("source_file must be a valid file path.", call. = FALSE)
   }
@@ -6,8 +8,27 @@
     stop("tar_dir must be a valid directory.", call. = FALSE)
   }
 
+
   tar_file <- fs::path_join(c(tar_dir, basename(source_file)))
   fs::file_copy(source_file, tar_file, overwrite = TRUE)
+
+  # if there is no YAML header, add prefer-html, because this helps with any
+  # function that returns HTML output, which should be supported by our
+  # documentation generators, since they get rendered to web.
+  x <- .readlines(tar_file)
+  first_non_empty <- x[which(!x == "")[1]]
+  if (!grepl("^---\\w*", first_non_empty)) {
+    x <- c(
+      "---",
+      "format:",
+      "  md:",
+      "    prefer-html: true",
+      "---",
+      "",
+      x
+    )
+    writeLines(x, tar_file)
+  }
 
   if (isTRUE(verbose)) {
     out <- try(quarto::quarto_render(
