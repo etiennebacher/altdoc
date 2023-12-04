6c6e60c422bc6cd963dab608342c9f2ddc18b695
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2023-12-04 05:37:31 -0500

    roxygen improvements

---
+++ man-roxygen/altdoc_preambles.R
@@ -0,0 +1,12 @@
+#' @section Altdoc preambles:
+#' 
+#' When you call `render_docs()`, `altdoc` will automatically paste the content of one of these three files to the top of a document:
+#' 
+#' * `altdoc/preamble_vignettes_qmd.yml`
+#' * `altdoc/preamble_vignettes_rmd.yml`
+#' * `altdoc/preamble_man_qmd.yml`
+#' 
+#' The README file uses the vignette preamble.
+#' 
+#' To preempt this behavior, add your own preamble to the README file or to a vignette.
+#' 
