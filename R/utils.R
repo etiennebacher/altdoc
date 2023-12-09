ff7362e4af54d6253bc8e66bc1dda2e0281bf434
Etienne Bacher <52219252+etiennebacher@users.noreply.github.com>
2023-12-09 13:11:15 +0100

    find the best python installation (#207)

--- R/utils.R
+++ R/utils.R
@@ -14,6 +14,12 @@
   .Platform$OS.type == "windows"
 }
 
+# this will give priority to the .venv folder if there is one in the package
+# root directory, and it will pick the more general installation otherwise
+.python_installation <- function() {
+  Sys.which("python")
+}
+
 # Is pip3 installed?
 .is_pip3 <- function() {
   x <- try(system2("pip3", args = "--version", stdout = TRUE, stderr = TRUE), silent = TRUE)
@@ -22,7 +28,7 @@
 
 # Is mkdocs installed?
 .is_mkdocs <- function() {
-  x <- try(system2("python3", args = "-m mkdocs", stdout = TRUE, stderr = TRUE), silent = TRUE)
+  x <- try(system2(.python_installation(), args = "-m mkdocs", stdout = TRUE, stderr = TRUE), silent = TRUE)
   return(!inherits(x, "try-error"))
 }
 
@@ -31,7 +37,6 @@
   x <- try(system2("sphinx-build", args = "--version", stdout = TRUE, stderr = TRUE), silent = TRUE)
   return(!inherits(x, "try-error"))
 }
-
 
 # https://stackoverflow.com/a/42945293/11598948
 .stop_quietly <- function() {
