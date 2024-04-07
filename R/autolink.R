fe2a4f89d56224e75e595c3f155cbdda7ae121ad
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2024-04-07 14:57:44 -0400

    do not modify home page, to pass tests

--- R/autolink.R
+++ R/autolink.R
@@ -4,10 +4,10 @@
     for (h in html_files) {
         downlit::downlit_html_path(h, h)
     }
-    h <- fs::path_join(c(path, "docs/index.html"))
-    if (fs::file_exists(h)) {
-        downlit::downlit_html_path(h, h)
-    }
+    # h <- fs::path_join(c(path, "docs/index.html"))
+    # if (fs::file_exists(h)) {
+    #     downlit::downlit_html_path(h, h)
+    # }
 
     md_files <- c(
         tryCatch(fs::dir_ls(fs::path_join(c(path, "docs/vignettes")), regexp = "\\.html$"), error = function(e) NULL),
@@ -15,8 +15,8 @@
     for (m in md_files) {
         downlit::downlit_html_path(m, m)
     }
-    m <- fs::path_join(c(path, "docs/README.md"))
-    if (fs::file_exists(m)) {
-        downlit::downlit_html_path(m, m)
-    }
+    # m <- fs::path_join(c(path, "docs/README.md"))
+    # if (fs::file_exists(m)) {
+    #     downlit::downlit_html_path(m, m)
+    # }
 }
