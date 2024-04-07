2e855148e5fc5e2f8f34de5ed3ddc08290c0d2d2
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2024-04-07 15:31:04 -0400

    autolink markdown

--- R/autolink.R
+++ R/autolink.R
@@ -10,10 +10,10 @@
     # }
 
     md_files <- c(
-        tryCatch(fs::dir_ls(fs::path_join(c(path, "docs/vignettes")), regexp = "\\.html$"), error = function(e) NULL),
-        tryCatch(fs::dir_ls(fs::path_join(c(path, "docs/man")), regexp = "\\.html$"), error = function(e) NULL))
+        tryCatch(fs::dir_ls(fs::path_join(c(path, "docs/vignettes")), regexp = "\\.md$"), error = function(e) NULL),
+        tryCatch(fs::dir_ls(fs::path_join(c(path, "docs/man")), regexp = "\\.md$"), error = function(e) NULL))
     for (m in md_files) {
-        downlit::downlit_html_path(m, m)
+        downlit::downlit_md_path(m, m)
     }
     # m <- fs::path_join(c(path, "docs/README.md"))
     # if (fs::file_exists(m)) {
