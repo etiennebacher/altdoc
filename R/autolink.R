1b7596c25774957cdfdc7bf0add026f2bd844a3c
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2024-04-07 16:08:43 -0400

    tryCatch

--- R/autolink.R
+++ R/autolink.R
@@ -1,8 +1,13 @@
 .autolink <- function(path = ".") {
+    # everyting wrapped in tryCatch() because especially error prone with weird pandoc memory errors
     html_files <- c(tryCatch(fs::dir_ls(fs::path_join(c(path, "docs/vignettes")), regexp = "\\.html$"), error = function(e) NULL),
                     tryCatch(fs::dir_ls(fs::path_join(c(path, "docs/man")), regexp = "\\.html$"), error = function(e) NULL))
     for (h in html_files) {
-        downlit::downlit_html_path(h, h)
+        tmp <- try(downlit::downlit_html_path(h, h), silent = TRUE)
+        if (inherits(tmp, "try-error")) {
+            cli::cli_alert_danger(sprintf("Failed to auto-link: %s", h))
+        }
+
     }
     # h <- fs::path_join(c(path, "docs/index.html"))
     # if (fs::file_exists(h)) {
@@ -13,7 +18,10 @@
         tryCatch(fs::dir_ls(fs::path_join(c(path, "docs/vignettes")), regexp = "\\.md$"), error = function(e) NULL),
         tryCatch(fs::dir_ls(fs::path_join(c(path, "docs/man")), regexp = "\\.md$"), error = function(e) NULL))
     for (m in md_files) {
-        downlit::downlit_md_path(m, m)
+        tmp <- try(downlit::downlit_md_path(m, m), silent = TRUE)
+        if (inherits(tmp, "try-error")) {
+            cli::cli_alert_danger(sprintf("Failed to auto-link: %s", h))
+        }
     }
     # m <- fs::path_join(c(path, "docs/README.md"))
     # if (fs::file_exists(m)) {
