fbb6b3dc8acad398fa9b2345ad21b155e5f2bd73
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2024-04-07 21:03:53 -0400

    test quarto autolink

--- tests/testthat/test-render_docs.R
+++ tests/testthat/test-render_docs.R
@@ -139,6 +139,39 @@ test_that("quarto: no error for basic workflow", {
 })
 
 
+test_that("quarto: autolink", {
+  skip_on_cran()
+  skip_if(.is_windows() && .on_ci(), "Windows on CI")
+
+  ### setup: create a temp package using the structure of testpkg.altdoc
+  path_to_example_pkg <- fs::path_abs(test_path("examples/testpkg.altdoc"))
+  create_local_project()
+  fs::dir_delete("R")
+  fs::dir_copy(path_to_example_pkg, ".")
+  all_files <- list.files("testpkg.altdoc", full.names = TRUE)
+  for (i in all_files) {
+    fs::file_move(i, ".")
+  }
+  fs::dir_delete("testpkg.altdoc")
+
+  ### generate docs
+  install.packages(".", repos = NULL, type = "source")
+  fs::file_move("README.Rmd", "README.qmd") # special thing quarto
+  setup_docs("quarto_website")
+  expect_no_error(render_docs(verbose = .on_ci(), autolink = TRUE))
+
+  ### Quarto output changes depending on the version, I don't have a solution for
+  ### now.
+
+  ### test
+  # expect_snapshot(.readlines("docs/index.html"))
+  # expect_snapshot(.readlines("docs/NEWS.html"))
+  # expect_snapshot(.readlines("docs/man/hello_base.html"))
+  # expect_snapshot(.readlines("docs/man/hello_r6.html"))
+  # expect_snapshot(.readlines("docs/vignettes/test.html"))
+})
+
+
 # Test failures ------------------------------
 
 test_that("render_docs errors if vignettes fail", {
