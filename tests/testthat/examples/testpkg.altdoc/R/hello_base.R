5cb905d1e2b2bd4e7e9565c4c0310986b108906e
Achim Zeileis <Achim.Zeileis@uibk.ac.at>
2024-07-04 11:43:44 +0200

    add @references with doi and @seealso for testing purposes

--- tests/testthat/examples/testpkg.altdoc/R/hello_base.R
+++ tests/testthat/examples/testpkg.altdoc/R/hello_base.R
@@ -5,6 +5,13 @@
 #' @return Some value
 #' @export
 #'
+#' @seealso \code{\link[base]{print}}, \code{\link{hello_r6}}
+#'
+#' @references Ihaka R, Gentleman R (1996).
+#'   R: A Language for Data Analysis and Graphics.
+#'   \emph{Journal of Computational and Graphical Statistics}. \bold{5}(3), 299--314.
+#'   \doi{10.2307/139080}
+#'
 #' @examples
 #' hello_base()
 hello_base <- function(x = 2) {
