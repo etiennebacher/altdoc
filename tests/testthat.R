df86946b66b824a4427b18c174e4ec4e3e902648
etiennebacher <etienne.bacher@protonmail.com>
2026-03-01 22:08:51 +0100

    fix the skip_on_cran

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,5 +1,14 @@
 library(testthat)
 library(altdoc)
 
-skip_on_cran()
-test_check("altdoc")
+# Taken from testthat:::on_cran()
+env <- Sys.getenv("NOT_CRAN")
+on_cran <- if (identical(env, "")) {
+    !interactive()
+} else {
+    !isTRUE(as.logical(env))
+}
+
+if (!on_cran) {
+    test_check("altdoc")
+}
