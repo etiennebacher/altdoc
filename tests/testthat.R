4c7dbb3e7abfa0dc2df4e64f3fda650539d51481
etiennebacher <etienne.bacher@protonmail.com>
2026-03-01 22:16:44 +0100

    more targeted skip_on_cran

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,14 +1,4 @@
 library(testthat)
 library(altdoc)
 
-# Taken from testthat:::on_cran()
-env <- Sys.getenv("NOT_CRAN")
-on_cran <- if (identical(env, "")) {
-    !interactive()
-} else {
-    !isTRUE(as.logical(env))
-}
-
-if (!on_cran) {
-    test_check("altdoc")
-}
+test_check("altdoc")
